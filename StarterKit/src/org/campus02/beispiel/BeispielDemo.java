66568eba553784c67dd230153692dd1cf40a9b59
denisedvorak <denise.dvorak@edu.campus02.at>
2016-02-04 17:33:23 +0100

    beispiel

---
+++ StarterKit/src/org/campus02/beispiel/BeispielDemo.java
@@ -0,0 +1,13 @@
+package org.campus02.beispiel;
+
+public class BeispielDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+		
+	}
+
+}
